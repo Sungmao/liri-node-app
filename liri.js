43d3b8fc8fabd4daa5e2d191495f700b799e99cf
Wu Sung-Mao <Allen@Wus-MacBook-Pro.local>
2016-10-15 21:04:08 -0500

    add spotify function

--- liri.js
+++ liri.js
@@ -1,8 +1,60 @@
 
 var request = require('request');
+var spotify = require('spotify');
 
 var input1 = process.argv[2];
 var input2 = process.argv[3];
+
+
+function thisSong(input2) {
+
+	var songName = input2;
+	var songQuery = { 
+		type: 'track',
+		query: songName + '&limit=10'
+		}
+
+	spotify.search(songQuery, function(err, data) {
+    	if ( err ) {
+       	 console.log('Error occurred: ' + err);
+       	 return;
+   	 	}
+
+   	 	songJson = data.tracks
+
+   	 	for (i = 0; i<songJson.items.length; i++) {
+    	
+    	console.log(songJson.items[i].artists[0].name);
+    	console.log(songJson.items[i].name);
+    	console.log(songJson.items[i].preview_url);
+    	console.log(songJson.items[i].album.name);
+    	console.log("--------------------------------")
+    	
+    	}
+
+	});
+}
+
+
+thisSong(input2);
+
+
+// This will show the following information about the song in your terminal/bash window
+
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// if no song is provided then your program will default to
+
+// "The Sign" by Ace of Base
+
+
+
+
+
+
+
 
 function movieThis(input2) {
 
@@ -29,6 +81,6 @@ function movieThis(input2) {
 
 }
 
-movieThis(input2);
+
 
 
